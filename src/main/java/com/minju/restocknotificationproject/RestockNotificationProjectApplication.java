002eb5d28e8ae904c4cef736a7f99c29969cf665
muchluvu <rorrxr@naver.com>
2024-12-14 11:17:40 +0900

    Initial commit

---
+++ src/main/java/com/minju/restocknotificationproject/RestockNotificationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.minju.restocknotificationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockNotificationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestockNotificationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minju/restocknotificationproject/RestockNotificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minju.restocknotificationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestockNotificationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
