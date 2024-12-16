f08fb8092b70be0bdba7d58621d56edfb6eb3c55
muchluvu <rorrxr@naver.com>
2024-12-16 10:05:06 +0900

    fix: controller url 수정

--- src/main/java/com/minju/restocknotificationproject/controller/NotificationController.java
+++ src/main/java/com/minju/restocknotificationproject/controller/NotificationController.java
@@ -10,19 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
 @RequiredArgsConstructor
 public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @PostMapping("/{productId}/notifications/re-stock")
+    @PostMapping("/products/{productId}/notifications/re-stock")
     public ResponseEntity<ProductNotificationHistoryResponseDto> sendRestockNotification(@PathVariable Long productId) {
         ProductNotificationHistoryResponseDto response = notificationService.sendRestockNotification(productId);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/admin/{productId}/notifications/re-stock")
+    @PostMapping("/admin/products/{productId}/notifications/re-stock")
     public ResponseEntity<ProductNotificationHistoryResponseDto> resendRestockNotification(@PathVariable Long productId) {
         ProductNotificationHistoryResponseDto response = notificationService.sendRestockNotification(productId);
         return ResponseEntity.ok(response);
